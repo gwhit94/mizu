a0d005f5a62fe3eb25841d2031b49ca5041e5387
Graham <greysnaa@gmail.com>
2019-08-19 10:54:24 -0500

    Generated components and API service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
